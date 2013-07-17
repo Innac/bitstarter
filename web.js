b3734d76673d69eeac4258a776d4b94f2f000625
Innac <miskozverys@gmail.com>
2013-07-17 18:02:51 +0000

    Added new function

--- web.js
+++ web.js
@@ -3,7 +3,17 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html, []);
+  response.send(
+      var fs = require('fs');
+      var content;
+      var data = fs.readFile('./index.html', function (err, data) {
+	  if (err) {
+	      throw err;
+	  }
+	  content = data;
+	  console.log(content);
+      };
+);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
